d90334d02255e43839d89da1e46ff221a7f3442c
Pedro Gustavo de Castro <pgkastro@gmail.com>
2022-08-02 14:12:51 -0300

    first commit

---
+++ src/br/com/fiap/tds/OlaMundo.java
@@ -0,0 +1,17 @@
+package br.com.fiap.tds;
+
+public class OlaMundo {
+
+	public static void main(String[] args) {
+		
+		int idade = 10;
+		double preco = 11.50;
+		char genero = 'M';
+		boolean maiorIdade = false;
+		
+		System.out.println(idade);
+		System.out.println(preco);
+		System.out.println(genero);
+		System.out.println(maiorIdade);
+	}
+}
